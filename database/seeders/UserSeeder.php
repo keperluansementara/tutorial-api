6f3da8e7fd3cea8e96af8b38e125908a15b88610
keperluansementara <keperluansementara@gmail.com>
2023-02-16 19:41:56 +0700

    update

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = [
+            'name' => 'Admin',
+            'email' => 'admin@api.com',
+            'password' => \bcrypt('12345'),
+        ];
+        User::create($user);
+    }
+}
